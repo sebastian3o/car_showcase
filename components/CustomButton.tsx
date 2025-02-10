4fed8f5d85513bba64e7379b1432ec84dbdbdc5a
sebastian3o <barbasebastian30@gmail.com>
2025-02-10 16:22:03 +0200

    Added navbar and footer

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -3,11 +3,11 @@ import { CustomButtonProps } from '@/types/index';
 import Image from 'next/image'; 
 
 const CustomButton =({title,containerStyles,
-    handleClick}:CustomButtonProps)=> {
+    handleClick,btnType}:CustomButtonProps)=> {
     return (
         <button 
         disabled={false}
-        type={'button'}
+        type={btnType || 'button'}
         className={`custom-btn ${containerStyles}`}
         onClick={handleClick}
         >
